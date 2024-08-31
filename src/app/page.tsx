69b247095853b7ac2d01ec7710b3e6e2e51d5eb6
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-08-31 14:59:11 -0300

    :dizzy: feat: add exemplo de animação simples com components custom (part 2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,14 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <motion.p
+          initial={{ opacity: 0, x: "-100%" } as Variant}
           animate={
             {
-              y: "50%",
-              color: "darkblue",
-              fontSize: "20px",
-              transition: { duration: 0.5 },
-              opacity: [0, 5, 1],
-              borderRadius: [0, 20, 50, 60, 80, 20, 40, 20, 0],
+              x: "0%",
+              opacity: 1,
             } as Variant
           }
+          transition={{ duration: 0.5 }}
           className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
         >
           Get started by editing&nbsp;
