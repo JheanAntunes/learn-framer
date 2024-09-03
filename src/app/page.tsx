359fbb242eea34baed91a04d72bf2a8e87e53f94
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-09-03 12:49:30 -0300

    :sparkles:	feat: add example de animação de saida com animatePresence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 import Image from "next/image";
 
-import { motion, Variant } from "framer-motion";
+import { motion, Variant, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+// Notificar quando o componente (motion) é desmontado ou está na árvore.
+// Adiar a desmontagem do componente (motion) até que a operação (animação) seja concluída.
 
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClickToggleVisible = () => setIsVisible(!isVisible);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button onClick={handleClickToggleVisible}>
+        {isVisible ? "Esconder" : "Mostrar"}
+      </Button>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <motion.p
           initial={{ opacity: 0, x: "-100%" } as Variant}
@@ -53,22 +65,31 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <AnimatePresence>
+          {isVisible && (
+            <motion.a
+              key="KEY"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Docs{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                Find in-depth information about Next.js features and API.
+              </p>
+            </motion.a>
+          )}
+        </AnimatePresence>
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
