8d68aa34f2a37950dced8991e6071d328de06c5a
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-09-05 15:10:56 -0300

    :sparkles:	feat: add example de animação com gestos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
-
-import { motion, Variant, AnimatePresence } from "framer-motion";
+import * as motion from "framer-motion/client";
+import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
@@ -18,15 +18,14 @@ export default function Home() {
       <Button onClick={handleClickToggleVisible}>
         {isVisible ? "Esconder" : "Mostrar"}
       </Button>
+
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <motion.p
-          initial={{ opacity: 0, x: "-100%" } as Variant}
-          animate={
-            {
-              x: "0%",
-              opacity: 1,
-            } as Variant
-          }
+          initial={{ opacity: 0, x: "-100%" }}
+          animate={{
+            x: "0%",
+            opacity: 1,
+          }}
           transition={{ duration: 0.5 }}
           className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
         >
@@ -125,11 +124,64 @@ export default function Home() {
           </p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <motion.div
+          whileHover={{
+            scale: 1.2,
+            backgroundColor: "rgb(0, 0, 0)",
+            color: "rgb(250, 250, 250)",
+            transition: { duration: 0.1 },
+          }}
+          onHoverStart={(event, info) => {
+            console.log("event:", event);
+            console.log("info:", info);
+            // Função de retorno de chamada que é acionada quando o ponteiro começa a passar o mouse sobre o componente.
+            // console.log("passei o mouse!");
+          }}
+          onHoverEnd={(event, info) => {
+            // Função de retorno de chamada que é acionada quando o ponteiro para de pairar sobre o componente.
+            // console.log("parei de passar sobre o componente o mouse!");
+          }}
+          whileTap={{ scale: 0.9, transition: { duration: 0.5 } }}
+          onTap={(event, info) => {
+            // Retorno de chamada quando o gesto de toque termina com êxito neste elemento
+            console.log("cliquei e terminei nesse componente na ui");
+          }}
+          onTapStart={(event, info) => {
+            // Retorno de chamada quando o gesto de toque é iniciado neste elemento
+            console.log("inicie o clique nesse componente na ui");
+          }}
+          onTapCancel={(event, info) => {
+            // Retorno de chamada quando o gesto de toque termina fora desse elemento.
+            console.log("soltei o clique fora deste componente na ui");
+          }}
+          className="group rounded-lg px-5 py-4"
+          drag
+          dragDirectionLock
+          onDirectionLock={(axis) => {
+            // A função de retorno de chamada que dispara uma direção de arrasto é determinada.
+            console.log(axis);
+          }}
+          dragConstraints={{ left: 0, top: 0, bottom: 0, right: 300 }}
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragTransition={{ bounceStiffness: 100, bounceDamping: 10 }}
+          onDrag={() => {
+            // Função de retorno de chamada que é acionada quando o componente é arrastado.
+            console.log("arrastei o elemento na ui");
+          }}
+          onDragStart={() => {
+            // Função de retorno de chamada que é acionada quando o arrastar é iniciado.
+            console.log("iniciei o arrastamento do elemento");
+          }}
+          onDragEnd={() => {
+            // Função de retorno de chamada que é acionada quando o arrasto termina.
+            console.log("parei de arrastar.");
+          }}
+          whileDrag={{
+            opacity: 0.8,
+            scale: 0.8,
+            transition: { duration: 0.5 },
+          }}
         >
           <h2 className="mb-3 text-2xl font-semibold">
             Deploy{" "}
@@ -140,7 +192,7 @@ export default function Home() {
           <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
+        </motion.div>
       </div>
     </main>
   );
