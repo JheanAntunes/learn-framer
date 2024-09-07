5125a1555da8302714bce1c9e456bd7ced7619b7
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-09-07 14:07:42 -0300

    :sparkles:	feat:  add example in home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Image from "next/image";
-import * as motion from "framer-motion/client";
-import { AnimatePresence, Variants } from "framer-motion";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import ExampleGesture from "./components/docs/example-gesture";
 import ExampleAnimatePresence from "./components/docs/example-animate-presence";
+import ExampleVariants from "./components/docs/example-variants";
+import ExampleOrchestration from "./components/docs/example-orchestration";
+import ExamplePropagation from "./components/docs/example-propagation";
 
 // Notificar quando o componente (motion) é desmontado ou está na árvore.
 // Adiar a desmontagem do componente (motion) até que a operação (animação) seja concluída.
@@ -20,8 +21,8 @@ export default function Home() {
       <Button onClick={handleClickToggleVisible}>
         {isVisible ? "Esconder" : "Mostrar"}
       </Button>
-
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <ExampleVariants />
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -42,17 +43,8 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      <ExamplePropagation />
+      <ExampleOrchestration />
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <ExampleAnimatePresence isVisible={isVisible} />
         <a
