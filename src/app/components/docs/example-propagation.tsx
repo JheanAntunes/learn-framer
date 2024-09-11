13f5840b9b2318557ae2d6b5cc27f115f5124a85
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-09-11 11:55:20 -0300

    :sparkles:	feat:  add example value dinamics in variants

--- src/app/components/docs/example-propagation.tsx
+++ src/app/components/docs/example-propagation.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import * as motion from "framer-motion/client";
+import { Variants } from "framer-motion";
 
 const ExamplePropagation = () => {
   const list = {
@@ -8,16 +9,24 @@ const ExamplePropagation = () => {
     hidden: { opacity: 0 },
   };
 
-  const item = {
-    visible: { opacity: 1, x: 0 },
+  const item: Variants = {
+    visible: (i: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: { delay: i * 0.3 },
+    }),
     hidden: { opacity: 0, x: -100 },
   };
 
+  const fakeArrayItems = [0, 1, 2];
+
   return (
     <motion.ul initial="hidden" animate="visible" variants={list}>
-      <motion.li variants={item}>Propagração </motion.li>
-      <motion.li variants={item}>Propagração </motion.li>
-      <motion.li variants={item}>Propagração</motion.li>
+      {fakeArrayItems.map((_, i) => (
+        <motion.li key={i} custom={i} variants={item}>
+          Propagração{" "}
+        </motion.li>
+      ))}
     </motion.ul>
   );
 };
