120fac4e7dfb4bda50074ba8e4c2b050e696c93f
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-09-14 13:40:30 -0300

    :truck:	chore: mover pastar

---
+++ src/components/docs/example-in-view.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ExampleInView = () => {
+  return (
+    <section>
+      <div className="h-svh bg-red-600">
+        <h1 className="">Anime</h1>
+      </div>
+      <div>
+        <h1>Quando</h1>
+      </div>
+      <div>
+        <h1>Estiver em</h1>
+      </div>
+      <div>
+        <h1>Exibição</h1>
+      </div>
+    </section>
+  );
+};
+
+export default ExampleInView;
