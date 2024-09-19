c5dd978fa9d03ef4eb48d70a9fb4f2a1787298b6
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-09-19 00:46:11 -0300

    :sparkles:	feat: add example hook useAnimate with useInView

--- src/components/docs/example-in-view.tsx
+++ src/components/docs/example-in-view.tsx
@@ -1,20 +1,21 @@
 "use client";
 import React, { useEffect, useRef } from "react";
 import * as motion from "framer-motion/client";
-import { useInView } from "framer-motion";
+import { useAnimate, useInView } from "framer-motion";
 const ExampleInView = () => {
-  const ref = useRef(null);
-  const inView = useInView(ref, { once: true });
+  const [scope, animate] = useAnimate();
+  const inView = useInView(scope, { once: true });
   useEffect(() => {
-    console.log("element está visievel ? ", inView);
-  }, [inView]);
+    if (inView) {
+      animate("h1", { opacity: 1, x: 0 }, { duration: 0.5 });
+    }
+  }, [inView, scope, animate]);
 
   return (
     <section className="w-full">
-      <motion.div className="h-svh w-full bg-red-600">
+      <motion.div ref={scope} className="h-svh w-full bg-red-600">
         <motion.h1
-          animate={{ opacity: inView ? 1 : 0, x: inView ? 0 : -200 }}
-          ref={ref}
+          initial={{ opacity: 0, x: -200 }}
           className="p-5 text-8xl"
           transition={{ duration: 0.5 }}
         >
